b375c4fa05ffca14027b01c87af097d6953d0c44
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-02-11 09:02:26 +0200

    Create ContactForm.jsx

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,88 @@
+import styles  from '../ContactForm/ContactForm.module.css';
+
+import {useState} from "react";
+
+import { useSelector, useDispatch } from 'react-redux';
+import {setInContacts} from '../../redux/contacts/contacts.slice';
+
+const getRandomID=()=> {
+  return `${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+export const ContactForm =()=> { 
+  const dispatch = useDispatch();
+  const contacts = useSelector(state => state.contacts.data);
+
+  const [name,setInName] = useState(''); 
+  const [number,setInNumber] = useState('');
+
+  const handleChange = event => {    
+    const { name, value } = event.target;    
+    name==='name'?setInName(value):setInNumber(value);      
+  };
+
+  const isContactInState = ({ name }) =>
+  !!contacts.filter(({name: prevName}) => {return prevName === name}).length;
+
+  const onSubmit = ({ name, number }) => { 
+    if (isContactInState({ name })) {
+      alert('Contact is in phonebook');
+      return;    
+    }   
+    
+    dispatch(
+      setInContacts(
+        {id:getRandomID(), name, number }   
+        ), 
+    );         
+  };
+
+  const handleFormSubmit = evt => {   
+    evt.preventDefault();    
+    onSubmit({ name, number });
+    setInName('');
+    setInNumber('');   
+  }; 
+   
+  return (
+    <form className={styles.contact_form} onSubmit={ handleFormSubmit}>
+      <div className="">
+        <label htmlFor="name" className="">
+            <p>Name</p>
+        </label>
+        <input
+          id="name"
+          type="text"
+          name="name"
+          onChange={handleChange}
+          value={name}
+          placeholder="Search name..."
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </div>
+      <div className="">
+        <label htmlFor="number" className="">
+          <p>Number</p>
+        </label>
+        <input
+          id="number"
+          type="tel"
+          name="number"
+          onChange={handleChange}
+          value={number}
+          placeholder="000-00-00"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </div>     
+
+      <button className={styles.button} type="submit">
+        Add contact
+      </button>
+    </form>      
+  );    
+}
+
