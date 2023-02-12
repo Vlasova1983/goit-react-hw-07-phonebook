ea13fb37e9f6e1a294bfe5c593cf886695427d85
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-02-12 15:20:21 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,32 @@
 import styles  from './App.module.css';
 
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchContact } from "../redux/contacts/operetions";
+
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 
 
-export const App  =()=> {    
+import { getError, getIsLoading } from "../redux/contacts/contacts.selectors";
+
+export const App  =()=> {  
+  const dispatch = useDispatch();
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
+
+  useEffect(() => {
+    dispatch(fetchContact());
+  }, [dispatch]);
+
   return (
     <div className={styles.conteiner}>
       <h1>Phonebook</h1>
         <ContactForm/>
-      <h2>Contacts</h2>
-        <Filter/>      
+      <h2>Contacts</h2>      
+        <Filter/> 
+       {isLoading && !error && <b>Request in progress...</b>}      
         <ContactList/>               
     </div>
   );     
