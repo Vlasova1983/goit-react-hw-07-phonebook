ffe2677ba3815b2e9572a82abd6dea0da74f7f54
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-02-12 15:20:25 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,33 +1,35 @@
 import styles  from '../ContactList/ContactList.module.css';
 
 import { useSelector, useDispatch } from 'react-redux';
-import {deleteContactsAction} from '../../redux/contacts/contacts.slice';
+import { deleteContact} from "../../redux/contacts/operetions";
+
+import { getContacts } from "../../redux/contacts/contacts.selectors";
+import { getFilter } from '../../redux/filter/filter.selector';
 
 export const ContactList = () => {
     const dispatch = useDispatch();
-    const filter = useSelector(state => state.contacts.filter);
-    const data = useSelector(state => state.contacts.data);  
-    
-    const  getFilterContact =()=> {    
-        return data.filter((contact)=>contact.name.toLowerCase().includes(filter));
+    const filter = useSelector(getFilter);
+    const data = useSelector(getContacts);    
+
+    const  getFilterContact =()=> {       
+      return data.filter((contact)=>contact.name.toLowerCase().includes(filter));
     };
-    
-    const contacts = getFilterContact();       
-  
-    const handleDelete = (id)=> {
-        
-      dispatch(deleteContactsAction(id.target.name));    
+      
+    const contacts=getFilterContact();  
+
+    const handleDelete = (id)=> {        
+      dispatch(deleteContact(id.target.name));    
     };
   
     return (
-        <div className={styles.contact_list}>            
-            {contacts.map(contact=>( 
-                <div key={contact.id} className={styles.link}>
-                  <li> {contact.name} : {contact.number}</li> 
-                  <button name ={contact.id} className={styles.button} onClick={handleDelete}>Delete</button>
-                </div>           
-            ))}         
-        </div>            
+      <div className={styles.contact_list}>            
+        {contacts.map(contact=>( 
+          <div key={contact.id} className={styles.link}>
+            <li> {contact.name} : {contact.number}</li> 
+              <button name ={contact.id} className={styles.button} onClick={handleDelete}>Delete</button>
+          </div>           
+        ))}         
+      </div>            
     );
   };
 
